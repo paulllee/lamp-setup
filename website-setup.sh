bbc0ced985da852405f97975353817d754d627c5
paulllee <paulllee.md@gmail.com>
2022-08-19 15:45:48 -0400

    Finished the rest of the script

--- website-setup.sh
+++ website-setup.sh
@@ -48,6 +48,84 @@ a2ensite $WEBSITE_DOMAIN_NAME.test.conf
 
 service apache2 restart
 
-cd ~
+cd
 
-echo "Returned to Home directory. Finished setting up $WEBSITE_ADDRESS"
+echo "Returning to Home directory; finished setting up $WEBSITE_ADDRESS"
+
+echo ''
+echo "Next: open up SourceTree and clone the $WEBSITE_ADDRESS repo into the public_html directory"
+echo ''
+echo 'When you are done press Enter'
+read USER_CHECKPOINT
+
+echo ''
+echo 'Then open up PuTTY or Windows Terminal and SSH as root on the client side (check passwork for credentials)'
+echo 'If you use Windows Terminal, here is how to SSH in:'
+echo ''
+echo 'ssh root@clientaddress'
+echo 'Enter password: (check PassWork)'
+echo 'cd /srv/www/dev.____.___/'
+echo 'mysqldump -u username -p username > databasename.sql'
+echo ''
+echo 'In some circumstances, you will need to use the --no-tablespaces flag:'
+echo 'mysqldump -u root -p rothman_dev > rothman_dev04062022.sql --no-tablespaces'
+echo ''
+echo 'When you are done press Enter'
+read USER_CHECKPOINT
+
+echo ''
+echo "FTP (using FileZilla) and download the sql file to the www/$WEBSITE_ADDRESS/ directory."
+echo 'Look through the htaccess and configuration.php files and modify the values to work for your local (ex: check any forcing for ssl, any directory paths...).'
+echo 'Also look through the gitignore file and download all of the files (a majority is media files) that have been ignored to your local.'
+echo ''
+echo 'When you are done press Enter'
+read USER_CHECKPOINT
+
+echo ''
+echo "Press Enter if you confirm the sql file is in the C:/.../Documents/www/$WEBSITE_ADDRESS/ directory"
+read USER_CHECKPOINT
+
+USER_VERIFIED=NO
+while [ "$USER_VERIFIED" != 'YES' ]
+do
+    echo ''
+    echo "If you mess up at any of these following steps, don't worry, I will ask to verify that the credentials are correct."
+
+    echo ''
+    echo 'Type in the MySQL database name for this site:'
+    read DATABASE_NAME
+
+    echo ''
+    echo 'Type in the MySQL database username for this site:'
+    read DATABASE_USER
+
+    echo ''
+    echo 'Type in the MySQL database password for this site:'
+    read DATABASE_PASS
+
+    echo ''
+    echo 'Type in the name of your sql file, including the .sql extension (ex: google_dev20220819.sql):'
+    read SQL_FILE
+
+    echo ''
+    echo 'Can you verify that all of the credentials you typed in are correct?'
+    echo 'YES for correct / NO for not correct (case sensitive):'
+    read USER_VERIFIED
+done
+
+cd /srv/www/$WEBSITE_ADDRESS/
+
+mysql --user="root" --password="ubuntu" --execute="CREATE DATABASE $DATABASE_NAME;"
+mysql --user="root" --password="ubuntu" --execute="CREATE USER '$DATABASE_USER'@localhost IDENTIFIED BY '$DATABASE_PASS';"
+mysql --user="root" --password="ubuntu" --execute="GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DATABASE_USER'@localhost;"
+
+mysql --user="$DATABASE_USER" -password="$DATABASE_PASS" < $SQL_FILE
+
+cd
+
+sudo service mysql start
+sudo service apache2 start
+sudo service apache2 restart
+
+echo ''
+echo 'The full installation of the website is now complete!'
